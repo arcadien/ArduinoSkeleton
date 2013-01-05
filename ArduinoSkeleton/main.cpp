955604a56649edf5ad09e966d67f2ab561526e09
Aurélien Labrosse <aurelien.labrosse@gmail.com>
2013-01-05 18:58:27 +0100

    Initial import

---
+++ ArduinoSkeleton/main.cpp
@@ -0,0 +1,35 @@
+
+
+// Arduino namespace begins in "arduino/" directory.
+// We need to prefix inclusion pathes
+//#include "arduino/include/Arduino.h"
+
+
+// Arduino libraries are also under Arduino namespace, in
+// their own "libraries" namespace.
+// Use "arduino/libraries/" prefix, like :
+//#include "arduino/libraries/SPI/SPI.h"
+
+// Also have a look in README to activate libraries source
+// code into compilation process.
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
+
+
+int main(void) {
+
+	setup();
+
+	for (;;) {
+		loop();
+	}
+
+	return 0;
+}
